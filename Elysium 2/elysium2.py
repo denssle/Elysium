2cb0e2b8c1b1e1ad4798ba71fb49003649e6f203
Dominik Enßle <dominik.enssle@innoq.com>
2013-07-24 09:17:32 +0200

    Elysium2: Init #2

---
+++ Elysium 2/elysium2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import tasche, kampf, held, monster
+
+class start_game:
+    def __init__(self, held):
+        pass
+    
+    def inventar_anlegen(self):
+        pass
+    
+    def menue(self):
+        pass
+    
+        
+class neues_spiel:
+    def __init__(self):
+        pass
+    
+    def laden_oder_neu(self):
+        pass
+    
+    def neuer(self):
+        return neuer_held
+    
+    def laden(self):
+        return geladener_held
+    
+if __name__ == "__main__":
+    held = neues_spiel()
+    start_game(held)
--- Elysium 2/kampf.py
+++ Elysium 2/kampf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Kampf:
-    def __init__(self, Held, Antagonis):
+    def __init__(self, Held, Antagonist):
         pass
         
         
